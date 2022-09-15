b562b758b5733d3ba757621e06cb660f652cdd0e
avenarek <90210709+avenarek@users.noreply.github.com>
2022-09-15 19:44:45 +0400

    Changed

--- TempClass.java
+++ TempClass.java
@@ -2,6 +2,5 @@ public class TempClass {
     public static void main(String[] args) {
         System.out.println("Hello Darkness My Old Friend");
         System.out.println("Hello");
-        System.out.println("bareeeeeeeev");
     }
 }
